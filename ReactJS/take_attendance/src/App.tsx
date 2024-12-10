dcba01f2536fcbfa515152fd67cc4f9e9ddb36ba
HuyTran0211 <quochuy2112002@gmail.com>
2024-12-10 14:40:14 +0700

    huy

--- ReactJS/take_attendance/src/App.tsx
+++ ReactJS/take_attendance/src/App.tsx
@@ -1,26 +1,21 @@
 import './styles/App.css'
-// import Button from "./ui/Button.tsx";
 import {BrowserRouter} from "react-router-dom";
 import Header from './components/Header.tsx'
+import Menu from "./pages/MenuPage.tsx";
 import Footer from './components/Footer.tsx'
-
 function App() {
-    // const buttonStyle = {
-    //     message: 'Save',
-    //     width: 150,
-    //     height: 50,
-    //     textSize: 20,
-    //     borderRadius: 50,
-    // }
 
 
   return (
     <>
         <BrowserRouter>
             <Header/>
-            {/*<Button {...buttonStyle}/>*/}
+            <main>
+                <Menu/>
+            </main>
             <Footer/>
         </BrowserRouter>
+
     </>
   )
 }
---
+++ ReactJS/take_attendance/src/pages/MenuPage.tsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import style from '../styles/Menu.module.css';
+import {Link} from "react-router-dom";
+
+function Menu() {
+    return (
+        <div className="container d-flex justify-content-center align-items-center">
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="text-center">
+                        <Link to="/TakeAttendancePage" className={`btn btn-outline-warning mx-2 ${style.buttonCustom}`}>Take Attendance</Link>
+                        <Link to="/AbsentReasonPage" className={`btn btn-outline-warning mx-2 ${style.buttonCustom}`}>Absent Reason</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
