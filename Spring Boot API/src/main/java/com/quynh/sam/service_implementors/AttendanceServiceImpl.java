881f8240885670974598b61e40475d01fe287215
KenTheWhale <kienquoc11026789@gmail.com>
2024-12-12 21:11:20 +0700

    quoc

--- Spring Boot API/src/main/java/com/quynh/sam/service_implementors/AttendanceServiceImpl.java
+++ Spring Boot API/src/main/java/com/quynh/sam/service_implementors/AttendanceServiceImpl.java
@@ -26,73 +26,125 @@ public class AttendanceServiceImpl implements AttendanceService {
 
     private final AttendanceRepo attendanceRepo;
 
-    @Override
-    public UpdateAttendanceStatusResponse updateAttendanceStatus(UpdateAttendanceStatusRequest request) {
-        List<Attendance> studentAttendanceList = attendanceRepo.findAll().stream()
-                .filter(attendance -> attendance.getDate().equals(LocalDate.now()))
-                .toList();
-
-        // Kiểm tra kích thước danh sách từ request và DB có khớp không
-        if (studentAttendanceList.size() != request.getStudentList().size()) {
-            return UpdateAttendanceStatusResponse.builder()
-                    .status("400")
-                    .message("The size of the student list does not match the number of attendance records.")
-                    .build();
-        }
-
-        // Use constants from the Status class
-        List<String> validStatuses = Arrays.asList(
-                Status.PRESENT,
-                Status.ABSENT,
-                Status.ABSENT_WITHOUT_REASON
-        );
-
-        Map<Integer, String> studentStatusMap = new HashMap<>();
-
-        for (UpdateAttendanceStatusRequest.Student student : request.getStudentList()) {
-            if (studentStatusMap.containsKey(student.getId())) {
-                return UpdateAttendanceStatusResponse.builder()
-                        .status("400")
-                        .message("Duplicate student ID found: " + student.getId())
-                        .build();
-            }
-            studentStatusMap.put(student.getId(), student.getStatus());
-        }
-
-     for (Attendance attendance : studentAttendanceList) {
-
-         String status = getStudentStatus(request, attendance.getStudent().getId());
-
-         // Check if student status is valid
-         if (status == null && !validStatuses.contains(status)) {
-             return UpdateAttendanceStatusResponse.builder()
-                     .status("400")
-                     .message("No status found for student ID " + attendance.getStudent().getId())
-                     .build();
-         }
-
-         attendance.setStatus(getStudentStatus(request, attendance.getStudent().getId()));
-     }
-
-        attendanceRepo.saveAll(studentAttendanceList);
-
-        return UpdateAttendanceStatusResponse.builder()
-                .status("200")
-                .message("Successfully updated attendance status")
-                .build();
-    }
-    private String getStudentStatus(UpdateAttendanceStatusRequest request, int id){
+//    @Override
+//    public UpdateAttendanceStatusResponse updateAttendanceStatus(UpdateAttendanceStatusRequest request) {
+//        List<Attendance> studentAttendanceList = attendanceRepo.findAll().stream()
+//                .filter(attendance -> attendance.getDate().equals(LocalDate.now()))
+//                .toList();
+//
+//        // Kiểm tra kích thước danh sách từ request và DB có khớp không
+//        if (studentAttendanceList.size() != request.getStudentList().size()) {
+//            return UpdateAttendanceStatusResponse.builder()
+//                    .status("400")
+//                    .message("The size of the student list does not match the number of attendance records.")
+//                    .build();
+//        }
+//
+//        // Use constants from the Status class
+//        List<String> validStatuses = Arrays.asList(
+//                Status.PRESENT,
+//                Status.ABSENT,
+//                Status.ABSENT_WITHOUT_REASON
+//        );
+//
+//        Map<Integer, String> studentStatusMap = new HashMap<>();
+//
+//        for (UpdateAttendanceStatusRequest.Student student : request.getStudentList()) {
+//            if (studentStatusMap.containsKey(student.getId())) {
+//                return UpdateAttendanceStatusResponse.builder()
+//                        .status("400")
+//                        .message("Duplicate student ID found: " + student.getId())
+//                        .build();
+//            }
+//            studentStatusMap.put(student.getId(), student.getStatus());
+//        }
+//
+//     for (Attendance attendance : studentAttendanceList) {
+//
+//         String status = getStudentStatus(request, attendance.getStudent().getId());
+//
+//         // Check if student status is valid
+//         if (status == null && !validStatuses.contains(status)) {
+//             return UpdateAttendanceStatusResponse.builder()
+//                     .status("400")
+//                     .message("No status found for student ID " + attendance.getStudent().getId())
+//                     .build();
+//         }
+//
+//         attendance.setStatus(getStudentStatus(request, attendance.getStudent().getId()));
+//     }
+//
+//        attendanceRepo.saveAll(studentAttendanceList);
+//
+//        return UpdateAttendanceStatusResponse.builder()
+//                .status("200")
+//                .message("Successfully updated attendance status")
+//                .build();
+//    }
+//    private String getStudentStatus(UpdateAttendanceStatusRequest request, int id){
 //        for(UpdateAttendanceStatusRequest.Student student : request.getStudentList()) {
 //            if(student.getId() == id) {
 //                return student.getStatus();
 //            }
 //        }
 //        return null;
-        return request.getStudentList().stream()
-                .filter(student -> student.getId() == id)
-                .map(UpdateAttendanceStatusRequest.Student::getStatus)
-                .findFirst()
-                .orElse(null);
+//        return request.getStudentList().stream()
+//                .filter(student -> student.getId() == id)
+//                .map(UpdateAttendanceStatusRequest.Student::getStatus)
+//                .findFirst()
+//                .orElse(null);
+//    }
+
+    @Override
+    public UpdateAttendanceStatusResponse updateAttendanceStatus(UpdateAttendanceStatusRequest request) {
+        List<Attendance> attendances = attendanceRepo.findAllByDate(LocalDate.now());// ds attendance hom nay
+        if(request.getStudentList().size() != attendances.size()) {
+            return UpdateAttendanceStatusResponse.builder()
+                    .status("400")
+                    .message("Error")
+                    .build();
+        }
+
+        for(UpdateAttendanceStatusRequest.Student student : request.getStudentList()) {
+            Attendance attendance = getAttendanceByStudentID(student.getId());
+            String status = checkStatus(student.getStatus());
+            if(status == null){
+                return UpdateAttendanceStatusResponse.builder()
+                        .status("400")
+                        .message("Error")
+                        .build();
+            }
+
+            assert attendance != null;
+            attendance.setStatus(status);
+            attendanceRepo.save(attendance);
+        }
+
+        return UpdateAttendanceStatusResponse.builder()
+                .status("200")
+                .message("Success")
+                .build();
+    }
+
+    private Attendance getAttendanceByStudentID(int id) {
+        List<Attendance> attendances = attendanceRepo.findAllByDate(LocalDate.now());
+        for(Attendance attendance : attendances) {
+            if(attendance.getStudent().getId() == id) {
+                return attendance;
+            }
+        }
+        return null;
+    }
+
+    private String checkStatus(String status) {
+        boolean isPresent = status.equalsIgnoreCase(Status.ABSENT);
+        boolean isAbsent = status.equalsIgnoreCase(Status.PRESENT);
+        boolean isAbsentWithoutReason = status.equalsIgnoreCase(Status.ABSENT_WITHOUT_REASON);
+
+        if(!isPresent && !isAbsent && !isAbsentWithoutReason){
+            return null;
+        }
+        return isPresent ? Status.PRESENT : (isAbsent ? Status.ABSENT : Status.ABSENT_WITHOUT_REASON);
     }
 
     @Override
